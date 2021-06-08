605342e9c20d589977df06169534ed2649c5348c
Alexander <alexander.shyrykalov@gmail.com>
2021-06-08 12:28:58 +0300

    add sort styles

--- src/components/Controls/SortType.jsx
+++ src/components/Controls/SortType.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeSortKey, selectSortKey } from "../../redux/users";
+import {
+  radioWrap,
+  radioGroup,
+  controlTitle,
+  controlItem,
+} from "../../styles/Controls.module.scss";
 
 export default function SortType() {
   const dispatch = useDispatch();
@@ -10,39 +16,42 @@ export default function SortType() {
     dispatch(changeSortKey(target.value));
   };
   return (
-    <div>
-      <div className="radio-wrapper">
-        <input
-          onChange={onChangeHandle}
-          type="radio"
-          id="sort-id"
-          value="id"
-          name="sortKey"
-          checked={sortKey === "id"}
-        />
-        <label htmlFor="sort-id">ID</label>
-      </div>
-      <div className="radio-wrapper">
-        <input
-          onChange={onChangeHandle}
-          type="radio"
-          id="sort-name"
-          value="name"
-          name="sortKey"
-          checked={sortKey === "name"}
-        />
-        <label htmlFor="sort-name">Имя</label>
-      </div>
-      <div className="radio-wrapper">
-        <input
-          onChange={onChangeHandle}
-          type="radio"
-          id="sort-age"
-          value="age"
-          name="sortKey"
-          checked={sortKey === "age"}
-        />
-        <label htmlFor="sort-age">Возраст</label>
+    <div className={controlItem}>
+      <p className={controlTitle}>Сортировка</p>
+      <div className={radioGroup}>
+        <div className={radioWrap}>
+          <input
+            onChange={onChangeHandle}
+            type="radio"
+            id="sort-id"
+            value="id"
+            name="sortKey"
+            checked={sortKey === "id"}
+          />
+          <label htmlFor="sort-id">ID</label>
+        </div>
+        <div className={radioWrap}>
+          <input
+            onChange={onChangeHandle}
+            type="radio"
+            id="sort-name"
+            value="name"
+            name="sortKey"
+            checked={sortKey === "name"}
+          />
+          <label htmlFor="sort-name">Имя</label>
+        </div>
+        <div className={radioWrap}>
+          <input
+            onChange={onChangeHandle}
+            type="radio"
+            id="sort-age"
+            value="age"
+            name="sortKey"
+            checked={sortKey === "age"}
+          />
+          <label htmlFor="sort-age">Возраст</label>
+        </div>
       </div>
     </div>
   );
